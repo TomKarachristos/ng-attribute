a945b2ed843f6886617df478dbf836861f3da48b
Tom Karachristos <TomKarachristos@users.noreply.github.com>
2016-09-28 22:53:47 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,45 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.isWorking = true;
+});
+
+app.directive('ngAttrs', function($compile) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attr ) {
+
+      scope.$watch( attr['ngAttrs'] , function (newAttrs) {
+        addAttributes(getAttributes(newAttrs));
+        compile();
+      }, true);
+
+      function getAttributes(newAttrs) {
+        var attrsToAdd = {};
+
+        if (angular.isObject(newAttrs)) {
+          angular.forEach(newAttrs, function(attrValue, attrName) {
+            if (typeof attrValue === "boolean") {
+              var fullAttribute = attrName.split('=');
+              if (attrValue) {
+                attrsToAdd[fullAttribute[0]] = fullAttribute[1];
+              }
+            }
+          });
+        } else {
+        	console.warn("no object in ng-attrs");
+        }
+
+        return attrsToAdd;
+      }
+
+      function addAttributes(attrsToAdd) {
+        angular.forEach(attrsToAdd, function(attrValue, attrName) {
+          element.attr(attrName, attrValue);
+        });
+      }
+      
+    }
+  }
+});
+
+
